d04c153f265a7bfec6c5afd260c718b5549444ad
Nouhack <nouh.saiche@gmail.com>
2022-11-13 22:54:32 +0100

    Init

---
+++ README.md
@@ -0,0 +1,56 @@
+<h1 style="text-align: center;">Fauth</h1>
+
+<p align="center">
+  <img src="https://drive.google.com/uc?id=1ElgsHenw7OrryqlOjrecebw6pmbexPCE">
+</p>
+<h1 style="text-align: center;"><span style="color: green">Fake</span> Auth solution </h1>
+<h5 style="text-align: center;">The lightest fake authentication solution only JS  . </h5>
+
+
+## Installation
+npm installation :
+
+```sh
+npm i @nouhsa/fauth
+```
+
+CDN :
+
+```sh
+<script src= "https://unpkg.com/@nouhsa/fauth@1.0.0/script.js"> </script>
+```
+## Functions list
+
+- sign_up ( username , email , password , image_profile )
+- sign_in ( email , password )
+- sign_out()
+- is_authenticated()
+- modify( new_user_data_object )
+- delete_account()
+
+
+
+
+## Release History
+
+* 1.0.0
+    * The first stable release .
+
+
+## Meta
+
+Contact me :tw-1f4e9: – [@FiX_0_](https://twitter.com/FiX_0_) – nouh.saiche@gmail.com .
+Donate (paypal) : :tw-25b6: nouh.saiche@gmail.com .
+
+  
+Distributed under the MIT license.
+
+## Contributing
+
+1. Fork it (<https://github.com/Nouhack/Fauth>)
+2. Create your feature branch (`git checkout -b feature/fooBar`)
+3. Commit your changes (`git commit -am 'Add some fooBar'`)
+4. Push to the branch (`git push origin feature/fooBar`)
+5. Create a new Pull Request
+
+
---
+++ src/index.ts
@@ -0,0 +1,89 @@
+interface user_data {
+  username: 'user';
+  email: string;
+  password: string;
+  image_profile: '';
+}
+// sign up function =========================
+export const sign_up = (user: user_data) => {
+  var the_new_user = {
+    username: user.username,
+    email: user.email,
+    password: user.password,
+    logged_in: true,
+    created_at: new Date(),
+    image_profile: user.image_profile,
+  };
+  window.localStorage.setItem('user', JSON.stringify(the_new_user));
+};
+// ==========================================
+// sign out function ========================
+export const sign_out = () => {
+  var authenticated_user = JSON.parse(window.localStorage.getItem('user')!);
+  var kk = {
+    ...authenticated_user,
+    logged_in: false,
+  };
+  window.localStorage.setItem('user', JSON.stringify(kk));
+};
+// ==========================================
+// sign in function =========================
+
+export const sign_in = (email: string, password: string) => {
+  var signed_user = JSON.parse(window.localStorage.getItem('user')!);
+  if (signed_user === null) {
+    return {
+      sign_in: false,
+      message: 'you should log in first',
+    };
+  } else {
+    // here verify the credentials
+    if (signed_user.email === email &&
+      signed_user.password === password
+    ) {
+      var pp = {
+        ...signed_user,
+        logged_in: true,
+      };
+      window.localStorage.setItem('user', JSON.stringify(pp));
+      return {
+        sign_in: true,
+        message: 'congrats you logged in',
+      };
+    } else {
+      return {
+        sign_in: false,
+        message: 'credential error',
+      }
+    }
+  }
+};
+// ==========================================
+// is authenticated function ================
+export const is_authenticated = () => {
+  var authenticated_user = JSON.parse(window.localStorage.getItem('user')!);
+  if (authenticated_user === null || !authenticated_user.logged_in) {
+    return authenticated_user;
+  }
+  return authenticated_user;
+};
+// ==========================================
+// delete account function ==================
+export const delete_account = () => {
+  window.localStorage.removeItem('user');
+};
+// ==========================================
+// modify account function ==================
+//
+export const modify = (newData: user_data) => {
+  var current_user = JSON.parse(window.localStorage.getItem('user')!);
+  var modified_user = {
+    username: newData.username ? newData.username : current_user.username,
+    email: newData.email ? newData.email : current_user.email,
+    password: newData.password ? newData.password : current_user.password,
+    created_at: current_user.created_at,
+    logged_in: true,
+    image_profile: newData.image_profile ? newData.image_profile : current_user.image_profile
+  };
+  window.localStorage.setItem('user', JSON.stringify(modified_user));
+};
